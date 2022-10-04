dbe50604341aff950d06ef171e56b505679504f0
Eugene Ignashov <eugene.ign@yandex.ru>
2022-10-04 15:51:23 +0500

    commit

--- src/main/java/com/example/springsecurity2/controller/AdminRest.java
+++ src/main/java/com/example/springsecurity2/controller/AdminRest.java
@@ -1,9 +1,16 @@
 package com.example.springsecurity2.controller;
 
+import com.example.springsecurity2.model.User;
 import com.example.springsecurity2.service.RoleService;
 import com.example.springsecurity2.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +23,24 @@ public class AdminRest {
     public AdminRest(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> showAllUsers() {
+        List<User> userList = userService.getAllUsers();
+        return userList.isEmpty()
+                ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                : new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        var user = userService.getUserById(userId);
+        return user == null
+                ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                : new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
